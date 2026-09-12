c4e7f988a9978c78816a459a6fbe79d3eb93203a
aaguy-hue <aaditmathur6@gmail.com>
2026-09-12 09:47:53 -0400

    remove old import

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -3,7 +3,6 @@
 import { FormEvent, useState } from "react";
 import { useRouter } from "next/navigation";
 
-import { supabase } from "../../lib/supabase/client";
 import { login, signup } from "../../lib/auth";
 
 import styles from "./page.module.css";
